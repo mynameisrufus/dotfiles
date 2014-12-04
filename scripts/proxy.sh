6757e69693a994f84a154a227bf405445cfbb3a5
Rufus Post <Rufus.Post@team.telstra.com>
2014-12-04 12:20:45 +1100

    Fix npm proxy var

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -7,14 +7,14 @@ run_proxy_configure()
 set_proxy()
 {
   proxy=${1:-http://localhost:3128}
-  run_proxy_configure npm config set http-proxy $proxy
+  run_proxy_configure npm config set proxy $proxy
   run_proxy_configure npm config set https-proxy $proxy
   run_proxy_configure export {all_proxy,http_proxy,https_proxy}=$proxy
 }
 
 unset_proxy()
 {
-  run_proxy_configure npm config delete http-proxy
+  run_proxy_configure npm config delete proxy
   run_proxy_configure npm config delete https-proxy
   run_proxy_configure unset {all_proxy,http_proxy,https_proxy}
 }
